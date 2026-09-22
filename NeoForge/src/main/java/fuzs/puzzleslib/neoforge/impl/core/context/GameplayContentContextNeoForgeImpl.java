a9272c460afdc80da1dec49ed6d9473a760953ac
Fuzss <28218241+Fuzss@users.noreply.github.com>
2026-09-22 22:12:17 +0200

    add back support for flattening and tilling

--- NeoForge/src/main/java/fuzs/puzzleslib/neoforge/impl/core/context/GameplayContentContextNeoForgeImpl.java
+++ NeoForge/src/main/java/fuzs/puzzleslib/neoforge/impl/core/context/GameplayContentContextNeoForgeImpl.java
@@ -16,35 +16,23 @@ import net.neoforged.neoforge.common.data.DataMapProvider;
 import net.neoforged.neoforge.registries.datamaps.DataMapType;
 import net.neoforged.neoforge.registries.datamaps.builtin.NeoForgeDataMaps;
 import net.neoforged.neoforge.registries.datamaps.builtin.Oxidizable;
-import net.neoforged.neoforge.registries.datamaps.builtin.Strippable;
+import net.neoforged.neoforge.registries.datamaps.builtin.Transformable;
 import net.neoforged.neoforge.registries.datamaps.builtin.Waxable;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.function.Function;
+import java.util.*;
 
 public final class GameplayContentContextNeoForgeImpl implements GameplayContentContext {
     private final Map<Holder<Block>, Flammable> flammables = new LinkedHashMap<>();
-    private final DataMapBuilder<Holder<Block>, Holder<Block>> strippables;
-    private final DataMapBuilder<Holder<Block>, Holder<Block>> oxidizables;
-    private final DataMapBuilder<Holder<Block>, Holder<Block>> waxables;
+    private final DataMapBuilder<Transformable> transformables;
+    private final DataMapBuilder<Oxidizable> oxidizables;
+    private final DataMapBuilder<Waxable> waxables;
     private final IEventBus eventBus;
 
     public GameplayContentContextNeoForgeImpl(String modId, IEventBus eventBus) {
         this.eventBus = eventBus;
-        this.strippables = new DataMapBuilder<>(modId,
-                NeoForgeDataMaps.STRIPPABLES,
-                Function.identity(),
-                (Holder<Block> holder) -> new Strippable(holder.value()));
-        this.oxidizables = new DataMapBuilder<>(modId,
-                NeoForgeDataMaps.OXIDIZABLES,
-                Function.identity(),
-                (Holder<Block> holder) -> new Oxidizable(holder.value()));
-        this.waxables = new DataMapBuilder<>(modId,
-                NeoForgeDataMaps.WAXABLES,
-                Function.identity(),
-                (Holder<Block> holder) -> new Waxable(holder.value()));
+        this.transformables = new DataMapBuilder<>(modId, NeoForgeDataMaps.TRANSFORMABLES);
+        this.oxidizables = new DataMapBuilder<>(modId, NeoForgeDataMaps.OXIDIZABLES);
+        this.waxables = new DataMapBuilder<>(modId, NeoForgeDataMaps.WAXABLES);
     }
 
     @Override
@@ -71,73 +59,94 @@ public final class GameplayContentContextNeoForgeImpl implements GameplayContent
     public void registerStrippable(Holder<Block> unstrippedBlock, Holder<Block> strippedBlock) {
         Objects.requireNonNull(unstrippedBlock, "unstripped block is null");
         Objects.requireNonNull(strippedBlock, "stripped block is null");
-        this.strippables.register(unstrippedBlock, strippedBlock);
+        this.transformables.register(unstrippedBlock, strippedBlock, (Holder<Block> input, Holder<Block> output) -> {
+            return Transformable.stripping(input.value(), output.value());
+        });
     }
 
     @Override
     public void registerFlattenable(Holder<Block> unflattenedBlock, Holder<Block> flattenedBlock) {
         Objects.requireNonNull(unflattenedBlock, "unflattened block is null");
         Objects.requireNonNull(flattenedBlock, "flattened block is null");
-        throw new UnsupportedOperationException();
+        this.transformables.register(unflattenedBlock, flattenedBlock, (Holder<Block> input, Holder<Block> output) -> {
+            return Transformable.flattening(input.value(), output.value());
+        });
     }
 
     @Override
     public void registerTillable(Holder<Block> untilledBlock, Holder<Block> tilledBlock) {
         Objects.requireNonNull(untilledBlock, "untilled block is null");
         Objects.requireNonNull(tilledBlock, "tilled block is null");
-        throw new UnsupportedOperationException();
+        this.transformables.register(untilledBlock, tilledBlock, (Holder<Block> input, Holder<Block> output) -> {
+            return Transformable.tilling(input.value(), output.value());
+        });
     }
 
     @Override
     public void registerOxidizable(Holder<Block> unoxidizedBlock, Holder<Block> oxidizedBlock) {
         Objects.requireNonNull(unoxidizedBlock, "unoxidized block is null");
         Objects.requireNonNull(oxidizedBlock, "oxidized block is null");
-        this.oxidizables.register(unoxidizedBlock, oxidizedBlock);
+        this.oxidizables.register(unoxidizedBlock, oxidizedBlock, (Holder<Block> _, Holder<Block> output) -> {
+            return new Oxidizable(output.value());
+        });
     }
 
     @Override
     public void registerWaxable(Holder<Block> unwaxedBlock, Holder<Block> waxedBlock) {
         Objects.requireNonNull(unwaxedBlock, "unwaxed block is null");
         Objects.requireNonNull(waxedBlock, "waxed block is null");
-        this.waxables.register(unwaxedBlock, waxedBlock);
+        this.waxables.register(unwaxedBlock, waxedBlock, (Holder<Block> _, Holder<Block> output) -> {
+            return new Waxable(output.value());
+        });
     }
 
-    private static class DataMapBuilder<K, V> {
-        private final Map<K, V> values = new LinkedHashMap<>();
+    private record Flammable(int encouragement, int flammability) {
+
+    }
+
+    static class DataMapBuilder<T> {
+        private final List<BlockTransform<T>> values = new ArrayList<>();
         private final String modId;
         private final DataProviderContext.Factory factory;
 
-        public <R, T> DataMapBuilder(String modId, DataMapType<R, T> dataMapType, Function<K, Holder<R>> keyConverter, Function<V, T> valueConverter) {
+        DataMapBuilder(String modId, DataMapType<Block, T> type) {
             this.modId = modId;
             this.factory = (DataProviderContext context) -> {
                 return new DataMapProvider(context.getPackOutput(), context.getRegistries()) {
                     @Override
                     protected void gather(HolderLookup.Provider registries) {
-                        Builder<T, R> builder = this.builder(dataMapType);
-                        DataMapBuilder.this.values.forEach((K key, V value) -> {
-                            builder.add(keyConverter.apply(key), valueConverter.apply(value), false);
-                        });
+                        Builder<T, Block> builder = this.builder(type);
+                        for (BlockTransform<T> transform : DataMapBuilder.this.values) {
+                            transform.build(builder);
+                        }
                     }
 
                     @Override
                     public String getName() {
-                        return super.getName() + " for " + ResourceKey.create(dataMapType.registryKey(),
-                                dataMapType.id());
+                        return super.getName() + " for " + ResourceKey.create(type.registryKey(), type.id());
                     }
                 };
             };
         }
 
-        public void register(K key, V value) {
+        void register(Holder<Block> input, Holder<Block> output, BlockTransformResult<T> result) {
             if (this.values.isEmpty()) {
                 DataProviderBuilder.of(this.modId, this.factory);
             }
 
-            this.values.put(key, value);
+            this.values.add(new BlockTransform<>(input, output, result));
         }
-    }
 
-    private record Flammable(int encouragement, int flammability) {
+        record BlockTransform<T>(Holder<Block> input, Holder<Block> output, BlockTransformResult<T> result) {
+            void build(DataMapProvider.Builder<T, Block> builder) {
+                T result = this.result().createResult(this.input(), this.output());
+                builder.add(this.input(), result, false);
+            }
+        }
 
+        @FunctionalInterface
+        interface BlockTransformResult<T> {
+            T createResult(Holder<Block> input, Holder<Block> output);
+        }
     }
 }
