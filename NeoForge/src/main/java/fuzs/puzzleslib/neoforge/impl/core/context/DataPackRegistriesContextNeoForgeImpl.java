4445ae155bd5ba5ff3310866287cb35aa7589ec3
Fuzss <28218241+Fuzss@users.noreply.github.com>
2026-09-26 15:53:46 +0200

    update NewDatapackRegistryEvent name

--- NeoForge/src/main/java/fuzs/puzzleslib/neoforge/impl/core/context/DataPackRegistriesContextNeoForgeImpl.java
+++ NeoForge/src/main/java/fuzs/puzzleslib/neoforge/impl/core/context/DataPackRegistriesContextNeoForgeImpl.java
@@ -4,25 +4,24 @@ import com.mojang.serialization.Codec;
 import fuzs.puzzleslib.common.api.core.v1.context.DataPackRegistriesContext;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
-import net.neoforged.neoforge.registries.DataPackRegistryEvent;
+import net.neoforged.neoforge.registries.NewDatapackRegistryEvent;
 
 import java.util.Objects;
 
-public record DataPackRegistriesContextNeoForgeImpl(DataPackRegistryEvent.NewRegistry event) implements DataPackRegistriesContext {
+public record DataPackRegistriesContextNeoForgeImpl(NewDatapackRegistryEvent event) implements DataPackRegistriesContext {
 
     @Override
     public <T> void registerWorldRegistry(ResourceKey<Registry<T>> registryKey, Codec<T> codec) {
         Objects.requireNonNull(registryKey, "registry key is null");
         Objects.requireNonNull(codec, "codec is null");
-        this.event.dataPackRegistry(registryKey, codec);
+        this.event.worldRegistry(registryKey, codec);
     }
 
     @Override
     public <T> void registerReloadableRegistry(ResourceKey<Registry<T>> registryKey, Codec<T> codec) {
         Objects.requireNonNull(registryKey, "registry key is null");
         Objects.requireNonNull(codec, "codec is null");
-        // TODO This should be registered as a reloadable registry when possible in NeoForge.
-        this.event.dataPackRegistry(registryKey, codec);
+        this.event.reloadableRegistry(registryKey, codec);
     }
 
     @Override
@@ -30,6 +29,6 @@ public record DataPackRegistriesContextNeoForgeImpl(DataPackRegistryEvent.NewReg
         Objects.requireNonNull(registryKey, "registry key is null");
         Objects.requireNonNull(codec, "codec is null");
         Objects.requireNonNull(networkCodec, "network codec is null");
-        this.event.dataPackRegistry(registryKey, codec, networkCodec);
+        this.event.worldRegistry(registryKey, codec, networkCodec);
     }
 }
